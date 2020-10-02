b37680054ac1102b8c1c1cb411e5277817bf94dc
Kamran Younis <join.kamran92@gmail.com>
2020-10-02 15:57:09 +0500

    Connect/Add replaced by Follow/Following feature.

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react'
 import {
   Image, Text, TouchableOpacity, ViewPropTypes
-} from 'react-native';
-import PropTypes from 'prop-types';
-import { moderateScale } from 'react-native-size-matters';
-import styles from './styles';
-import { AppStyles } from '../../themes';
-import RadioButton from '../RadioButton/RadioButton';
-import { CustomButton } from '../index';
-import { getReqTitle } from '../../utils/sharedUtils';
-import { FRIEND_STATUSES } from '../../constants/constants';
-import PlaceholderItem from '../PlaceholderItem';
+} from 'react-native'
+import PropTypes from 'prop-types'
+import { moderateScale } from 'react-native-size-matters'
+import styles from './styles'
+import { AppStyles } from '../../themes'
+import RadioButton from '../RadioButton/RadioButton'
+import { CustomButton } from '../index'
+import { getReqDetails, getReqTitle } from '../../utils/sharedUtils'
+import { FRIEND_STATUSES } from '../../constants/constants'
+import PlaceholderItem from '../PlaceholderItem'
+import I18n from '../../I18n'
+import { logToConsole } from '../../utils/logUtils'
 
-function ViewContactItem(props) {
+function ViewContactItem (props) {
   const {
     item = {}, onPress, loading, loggedInUser = {}, isRadioVisible = false, onPressContact, friendId, onDeleteRequest, fetching
-  } = props;
-  const {
-    picture, name, id: contactId, friend
-  } = item;
-  const { id: loggedInUserId = '' } = loggedInUser || {};
-  const { id: requestFriendId = '', status, sender: requestSender = loggedInUserId } = friend || {};
-  const activeOpacity = typeof onPress === 'function' ? 1 : 0.2;
-  const contactIcon = picture ? { uri: picture } : AppStyles.iconSet.profileFilled;
+  } = props
+  const { picture, name, id: contactId, friend } = item
+  const { id: loggedInUserId = '' } = loggedInUser || {}
+  const { id: requestFriendId = '', status, sender: requestSender = loggedInUserId } = friend || {}
 
-  const isNotSelf = loggedInUserId !== contactId;
-  const isSender = loggedInUserId === requestSender;
-  const isNotFriend = status !== FRIEND_STATUSES.FRIEND;
+  const activeOpacity = typeof onPress === 'function' ? 1 : 0.2
+  const contactIcon = picture ? { uri: picture } : AppStyles.iconSet.profileFilled
+
+  const isNotSelf = loggedInUserId !== contactId
+  const isSender = loggedInUserId === requestSender
 
   const onPressRequest = () => {
+
     if (!status || status === FRIEND_STATUSES.DELETED) {
-      onPress(contactId);
-    } else if (status === FRIEND_STATUSES.PENDING) {
-      onDeleteRequest(requestFriendId);
+      onPress(contactId)
+    } else if (status === FRIEND_STATUSES.PENDING || status === FRIEND_STATUSES.FRIEND) {
+      onDeleteRequest(requestFriendId, contactId)
     }
-  };
+  }
 
   if (fetching) {
-    return <PlaceholderItem />;
+    return <PlaceholderItem/>
   }
+
+  const { title, isHollow } = getReqDetails(status)
 
   return (
     <TouchableOpacity
@@ -51,31 +54,32 @@ function ViewContactItem(props) {
         style={styles.imageContainer}
         defaultSource={AppStyles.iconSet.profileFilled}
       />
-      <Text style={[styles.nameStyle, props.nameStyle]}>{ name }</Text>
-      { isRadioVisible && (
-      <RadioButton
-        isActive={props.isRadio}
-        circleStyle={styles.radioStyle}
-        size={moderateScale(37)}
-        color={AppStyles.colorSet.pinkI}
-      />
+      <Text style={[styles.nameStyle, props.nameStyle]}>{name}</Text>
+      {isRadioVisible && (
+        <RadioButton
+          isActive={props.isRadio}
+          circleStyle={styles.radioStyle}
+          size={moderateScale(37)}
+          color={AppStyles.colorSet.pinkI}
+        />
       )}
-      {!isRadioVisible && isNotSelf && isSender && (
-      <CustomButton
-        size="small"
-        hollow={!!friend && status !== FRIEND_STATUSES.DELETED}
-        onPress={onPressRequest}
-        loadingIndicatorColor={AppStyles.colorSet.pink}
-        isLoading={(loading && String(friendId) === String(contactId))}
-        title={getReqTitle(status)}
-        backgroundColor={AppStyles.colorSet.purple}
-      />
-      ) }
+      {!isRadioVisible && isNotSelf && (
+        <CustomButton
+          size="small"
+          hollow={isHollow}
+          onPress={onPressRequest}
+          container={styles.followButtonContainer}
+          loadingIndicatorColor={AppStyles.colorSet.pink}
+          isLoading={(loading && String(friendId) === String(contactId))}
+          title={I18n.t(title)}
+          backgroundColor={AppStyles.colorSet.purple}
+        />
+      )}
     </TouchableOpacity>
-  );
+  )
 }
 
-export default ViewContactItem;
+export default ViewContactItem
 
 ViewContactItem.propTypes = {
   item: PropTypes.object,
@@ -84,7 +88,7 @@ ViewContactItem.propTypes = {
   isRadio: PropTypes.bool,
   nameStyle: ViewPropTypes.style,
   onPressContact: PropTypes.func,
-};
+}
 
 ViewContactItem.defaultProps = {
   item: {},
@@ -93,4 +97,4 @@ ViewContactItem.defaultProps = {
   isRadio: false,
   nameStyle: {},
   onPressContact: () => {}
-};
+}
--- styles.js
+++ styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native';
 import { moderateScale, scale } from 'react-native-size-matters';
-import { AppStyles } from '../../themes';
+import { AppStyles, MetricsMod } from '../../themes'
 
 export default StyleSheet.create({
   itemContainer: {
@@ -28,5 +28,11 @@ export default StyleSheet.create({
     width: moderateScale(30),
     height: moderateScale(30),
     backgroundColor: 'rgb(243,244,250)'
-  }
+  },
+  followButtonContainer: {
+    minWidth: MetricsMod.hundredTen,
+    height: MetricsMod.twentyEight,
+    borderWidth: 1,
+    borderColor: AppStyles.colorSet.purple,
+  },
 });
